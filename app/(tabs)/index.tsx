73d60bcfbcfe2a7ffccc153538bd31f532cf79ec
abiodun emma <abiodunemma769@gmail.com>
2024-12-13 21:43:24 +0100

    api intergration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,13 @@ import { Stack } from 'expo-router'
 import React from 'react'
 import Colors from '@/constants/Colors';
 import { StyleSheet, Text, Touchable, TouchableOpacity, View , Image} from 'react-native'
+import {useHeaderHeight} from '@react-navigation/elements';
+import { TextInput } from 'react-native-gesture-handler';
+import { SearchBar } from 'react-native-screens';
+
 
 const Index = () => {
+  const headerHeight = useHeaderHeight();
   return (
     <>
  <Stack.Screen options={{ 
@@ -36,8 +41,21 @@ source={{
   ),
   }} 
   />
-  <View style={styles.container}>
-    <Text>Explore The Beautiful World!</Text>
+  <View style={[styles.container, {paddingTop: headerHeight}]}>
+    <Text style={styles.headingTxt}>Explore The Beautiful World!</Text>
+   <View style={styles.searchSectionWrapper}>
+    <View style={styles.SearchBar}>
+   <Ionicons name='search' 
+   size={18}
+   style={{ marginRight: 5 }} 
+   color={Colors.black}
+   />
+   <TextInput placeholder='Search.....' />
+   </View>
+   <TouchableOpacity onPress={() => {styles.filterBtn}}>
+    <Ionicons name='options' size={28} color={Colors.white} />
+   </TouchableOpacity>
+  </View>
   </View>
   </>
   )
@@ -48,8 +66,32 @@ export default Index
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  
-  
+    paddingHorizontal: 20,
+    backgroundColor: Colors.bgColor,
+  },
+  headingTxt: {
+fontSize: 20,
+fontWeight: '800',
+color:  Colors.black,
+marginTop: 10,
+  },
+  searchSectionWrapper: {
+    flexDirection: 'row',
+    marginVertical: 20,
+
+
+  },
+  SearchBar: {
+flexDirection: 'row',
+backgroundColor: Colors.white,
+padding: 16,
+borderRadius: 10,
+flex: 1,
+
+  },
+  filterBtn:{
+    backgroundColor:Colors.primaryColor
 
   }
+
 })
